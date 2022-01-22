af8c3a6c59e3dd4112ca5ab55d8418cd401d4ef3
VincentF-a <fanvincent2@gmail.com>
2022-01-22 23:15:52 +0100

    Create pyramid_slide.py
    added pyramid slide

---
+++ tier4/pyramid_slide.py
@@ -0,0 +1,40 @@
+"""
+TODO: list de list
+How to optimize the path taken
+"""
+
+
+def longest_slide_down(pyramid):
+    path_total = pyramid[0][0]
+    pyramid.pop(0)
+    index = 0
+    for list_py in pyramid:
+        # TODO: we consider that if both value are the same we take the right one
+        if list_py[index] > list_py[index + 1]:
+            path_total += list_py[index]
+            index = index
+        else:
+            path_total += list_py[index + 1]
+            index += 1
+    return path_total
+
+
+# Sample
+# Should equal 1074
+middle_pyramid = [
+    [75],
+    [95, 64],
+    [17, 47, 82],
+    [18, 35, 87, 10],
+    [20, 4, 82, 47, 65],
+    [19, 1, 23, 75, 3, 34],
+    [88, 2, 77, 73, 7, 63, 67],
+    [99, 65, 4, 28, 6, 16, 70, 92],
+    [41, 41, 26, 56, 83, 40, 80, 70, 33],
+    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+    [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
+]
